4987a508895e06d35fb425a8aa74f9760ab4eea0
viseshrp <viseshrprasad@gmail.com>
2023-01-02 13:44:46 -0500

    remove prompts

--- fbn/cli.py
+++ fbn/cli.py
@@ -5,89 +5,83 @@ from .fb import check_and_notify
 from . import __version__
 
 
-@click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.version_option(__version__, '-v', '--version', )
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.version_option(
+    __version__,
+    "-v",
+    "--version",
+)
 @click.option(
-    '--group',
-    'target',
-    flag_value='group',
+    "--group",
+    "target",
+    flag_value="group",
     default=True,
-    help="Monitor a facebook group"
+    help="Monitor a Facebook group",
 )
+@click.option("--page", "target", flag_value="page", help="Monitor a Facebook page")
 @click.option(
-    '--page',
-    'target',
-    flag_value='page',
-    help="Monitor a facebook page"
-)
-@click.option(
-    '-i',
-    '--id',
-    'target_id',
+    "-i",
+    "--id",
+    "target_id",
+    type=str,
     required=True,
-    type=str,
-    help="The facebook group/page name or id"
+    help="The Facebook group/page name or id",
 )
 @click.option(
-    '-u',
-    '--username',
-    prompt="Please enter your facebook username",
-    hide_input=False,
-    confirmation_prompt=True,
-    required=False,
+    "-u",
+    "--username",
     type=str,
+    default="",
     envvar="FBN_FB_USERNAME",
-    help="Your facebook username"
+    help="Your Facebook username",
 )
 @click.option(
-    '-p',
-    '--password',
-    prompt="Please enter your facebook password",
-    hide_input=True,
-    confirmation_prompt=True,
-    required=False,
+    "-p",
+    "--password",
     type=str,
-    envvar="FBN_PASSWORD",
-    help="Your facebook password"
+    default="",
+    envvar="FBN_FB_PASSWORD",
+    help="Your Facebook password",
 )
 @click.option(
-    '-c',
-    '--cookies-file',
-    type=click.Path(exists=True, file_okay=True, dir_okay=False,
-                    readable=True, resolve_path=True, allow_dash=False),
+    "-c",
+    "--cookies-file",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        allow_dash=False,
+    ),
     required=False,
-    help="Path to the facebook cookies file"
+    help="Path to the Facebook cookies file",
 )
 @click.option(
-    '-e',
-    '--every',
-    'frequency',
+    "-e",
+    "--every",
+    "frequency",
     type=str,
     default="1h",
     show_default=True,
-    help="Monitor frequency"
+    help="Monitor frequency",
 )
 @click.option(
-    '-a',
-    '--apprise-url',
-    prompt="Please paste your apprise URL here (hidden)",
-    hide_input=True,
-    confirmation_prompt=True,
-    required=True,
+    "-a",
+    "--apprise-url",
     type=str,
+    required=True,
     envvar="FBN_APPRISE_URL",
-    help="The apprise URL to notify"
+    help="The apprise URL to notify",
 )
 @click.option(
-    '-v',
-    '--verbose',
-    is_flag=True,
-    required=False,
-    help="Enable debug logging."
+    "-v", "--verbose", is_flag=True, default=False, help="Enable debug logging."
 )
-def main(*args):
+def main(
+        target, target_id, username, password, cookies_file, frequency, apprise_url, verbose
+):
     """
-    Simple CLI tool to look for new posts in a facebook group or page and
+    Simple CLI tool to look for new posts in a Facebook group or page and
     then send you a notification. Public pages/groups do not require authentication information.
 
     Example usage:
@@ -98,7 +92,16 @@ def main(*args):
         --apprise-url mailto://gmailusername:gmailpassword@gmail.com
     """
     try:
-        check_and_notify(*args)
+        check_and_notify(
+            target,
+            target_id,
+            username,
+            password,
+            cookies_file,
+            frequency,
+            apprise_url,
+            verbose
+        )
     except Exception as e:
         # all other exceptions
         click.echo(e)
